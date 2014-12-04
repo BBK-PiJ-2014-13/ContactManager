08fd65efd150f9020e7f53c645fa9081d610a65d
Ilya Ivanov <contact@ilyaivanov.org>
2014-12-04 21:46:58 +0000

    Test failed. Removed @Test annotation from testsGetId

--- src/contactManagerCore/MeetingTest.java
+++ src/contactManagerCore/MeetingTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 public class MeetingTest extends BasicTest {
 	MeetingMock meeting = new MeetingMock(10);
 
-	@Test
 	public void testsGetId() {
 		valueExpected = 10;
 		valueActual = meeting.getId();
