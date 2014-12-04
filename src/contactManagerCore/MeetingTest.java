1b2e6f14c426065d00e41ed408076824518d2d9b
Ilya Ivanov <contact@ilyaivanov.org>
2014-12-04 22:04:50 +0000

    Added body and @Test to testsGetContacts()

--- src/contactManagerCore/MeetingTest.java
+++ src/contactManagerCore/MeetingTest.java
@@ -2,12 +2,14 @@ package contactManagerCore;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.HashSet;
 
 import org.junit.Test;
 
 public class MeetingTest extends BasicTest {
+	HashSet<Contact> sc = new HashSet<Contact>();
 	GregorianCalendar gc = new GregorianCalendar();
-	MeetingMock meeting = new MeetingMock(10, gc);
+	MeetingMock meeting = new MeetingMock(10, gc, sc);
 
 	public void testsGetId() {
 		valueExpected = 10;
@@ -20,9 +22,11 @@ public class MeetingTest extends BasicTest {
 		valueActual = meeting.getDate();
 		test();
 	}
-	
+
 	@Test
 	public void testsGetContacts() {
-		
+		valueExpected = sc;
+		valueActual = meeting.getContacts();
+		test();
 	}
 }
