1cdf98dd7d2a10d85d91c69f611967c90dbcb109
Ilya Ivanov <contact@ilyaivanov.org>
2014-12-04 21:05:13 +0000

    Copied class BasicTest for testing purposes

---
+++ src/contactManagerCore/BasicTest.java
@@ -0,0 +1,16 @@
+package contactManagerCore;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+
+public class BasicTest {
+	Object valueExpected;
+	Object valueActual;
+	
+	@Test
+	public void test() {
+		assertEquals(valueExpected, valueActual);
+	}
+}
