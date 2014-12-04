079874def3167141f1f2a4ed96e707616544ae30
Ilya Ivanov <contact@ilyaivanov.org>
2014-12-04 21:35:33 +0000

    Made body for method testsGetId

--- src/contactManagerCore/MeetingTest.java
+++ src/contactManagerCore/MeetingTest.java
@@ -5,8 +5,12 @@ import java.util.Calendar;
 import org.junit.Test;
 
 public class MeetingTest extends BasicTest {
+	Meeting meeting = new Meeting(10);
 
+	@Test
 	public void testsGetId() {
-
+		valueExpected = 10;
+		valueActual = meeting.getId();
+		test();
 	}
 }
