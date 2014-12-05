12e550bcd4b926eedbe0765c1fd383fa4d733000
Ilya Ivanov <contact@ilyaivanov.org>
2014-12-05 21:33:09 +0000

    Added field name and modified constructor

--- src/contactManagerCore/ContactImpl.java
+++ src/contactManagerCore/ContactImpl.java
@@ -2,9 +2,11 @@ package contactManagerCore;
 
 public class ContactImpl implements Contact {
 	private int id;
+	private String name;
 	
 	public ContactImpl(int id, String name) {
 		this.id = id;
+		this.name = name;
 	}
 
 	@Override
