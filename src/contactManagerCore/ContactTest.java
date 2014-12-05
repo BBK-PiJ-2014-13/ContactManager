439dfff0427adc22281ac5e55ee001740030e58d
Ilya Ivanov <contact@ilyaivanov.org>
2014-12-05 21:44:42 +0000

    Didn't change anything

--- src/contactManagerCore/ContactTest.java
+++ src/contactManagerCore/ContactTest.java
@@ -4,25 +4,25 @@ import org.junit.Test;
 
 public class ContactTest extends BasicTest {
 	ContactImpl contact = new ContactImpl(10, "John");
-	
+
 	public void testsGetId() {
 		valueExpected = 10;
 		valueActual = contact.getId();
 		test();
 	}
-	
+
 	public void testsGetName() {
 		valueExpected = "John";
 		valueActual = contact.getName();
 		test();
 	}
-	
+
 	public void testsGetNotes() {
-	valueExpected = "";
-	valueActual = contact.getNotes();
-	test();
+		valueExpected = "";
+		valueActual = contact.getNotes();
+		test();
 	}
-	
+
 	public void testsAddNotes() {
 		contact.addNotes("notes");
 		valueExpected = "notes";
