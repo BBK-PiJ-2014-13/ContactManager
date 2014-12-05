8e076d0880cf0c069cc4f07db19c50d981c7a55b
Ilya Ivanov <contact@ilyaivanov.org>
2014-12-05 20:50:22 +0000

    Added String to constructor

--- src/contactManagerCore/PastMeetingImpl.java
+++ src/contactManagerCore/PastMeetingImpl.java
@@ -5,7 +5,7 @@ import java.util.Set;
 
 public class PastMeetingImpl extends MeetingImpl implements PastMeeting{
 
-	public PastMeetingImpl(int i, Calendar cal, Set<Contact> con) {
+	public PastMeetingImpl(int i, Calendar cal, Set<Contact> con, String not) {
 		super(i, cal, con);
 	}
 
