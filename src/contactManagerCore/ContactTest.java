e00a09e9c400031f040c68ab8cd9fe3d3168c9ca
Ilya Ivanov <contact@ilyaivanov.org>
2014-12-05 21:23:38 +0000

    Added testsGetId()

--- src/contactManagerCore/ContactTest.java
+++ src/contactManagerCore/ContactTest.java
@@ -1,7 +1,13 @@
 package contactManagerCore;
 
+import org.junit.Test;
+
 public class ContactTest extends BasicTest {
 	ContactImpl contact = new ContactImpl(10, "John");
 	
-
+	public void testsGetId() {
+		valueExpected = 10;
+		valueActual = contact.getId();
+		test();
+	}
 }
