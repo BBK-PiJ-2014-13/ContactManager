1bcbe60854c1cec7de82c5e2690f6c2433ad9453
Ilya Ivanov <contact@ilyaivanov.org>
2014-12-05 20:54:35 +0000

    Added field notes

--- src/contactManagerCore/PastMeetingImpl.java
+++ src/contactManagerCore/PastMeetingImpl.java
@@ -4,14 +4,14 @@ import java.util.Calendar;
 import java.util.Set;
 
 public class PastMeetingImpl extends MeetingImpl implements PastMeeting{
-
-	public PastMeetingImpl(int i, Calendar cal, Set<Contact> con, String not) {
+	String notes;
+	
+	public PastMeetingImpl(int i, Calendar cal, Set<Contact> con, String notes) {
 		super(i, cal, con);
+		this.notes = notes;
 	}
 
-	@Override
 	public String getNotes() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
