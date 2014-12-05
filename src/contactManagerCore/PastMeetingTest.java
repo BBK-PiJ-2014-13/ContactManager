389014254a9aba031d8a4ff2e31b5733512a4d51
Ilya Ivanov <contact@ilyaivanov.org>
2014-12-05 20:51:04 +0000

    Added soe fields

--- src/contactManagerCore/PastMeetingTest.java
+++ src/contactManagerCore/PastMeetingTest.java
@@ -1,8 +1,15 @@
 package contactManagerCore;
 
+import java.util.GregorianCalendar;
+import java.util.HashSet;
+
 import org.junit.Test;
 
 public class PastMeetingTest extends BasicTest{
+	HashSet<Contact> contact = new HashSet<Contact>();
+	GregorianCalendar calendar = new GregorianCalendar();
+	String notes = "notes";
+	PastMeetingImpl meeting = new PastMeetingImpl(10, calendar, contact, notes);
 	
 	@Test
 	public void testsGetNotes() {
