1563d6d460b1652a679c76e649c1436fe3c854be
Ilya Ivanov <contact@ilyaivanov.org>
2014-12-06 22:00:45 +0000

    Made field "notes" private

--- src/contactManagerCore/PastMeetingImpl.java
+++ src/contactManagerCore/PastMeetingImpl.java
@@ -4,7 +4,7 @@ import java.util.Calendar;
 import java.util.Set;
 
 public class PastMeetingImpl extends MeetingImpl implements PastMeeting{
-	public String notes;
+	private String notes;
 	
 	public PastMeetingImpl(int id, Calendar calendar, Set<Contact> contacts, String notes) {
 		super(id, calendar, contacts);
