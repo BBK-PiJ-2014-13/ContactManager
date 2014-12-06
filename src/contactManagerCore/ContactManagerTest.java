430a92aeff07f19991b05d9b013df0a6d593b53f
Ilya Ivanov <contact@ilyaivanov.org>
2014-12-06 18:08:15 +0000

    Added a line to testAddFutureMeeting()

--- src/contactManagerCore/ContactManagerTest.java
+++ src/contactManagerCore/ContactManagerTest.java
@@ -3,13 +3,16 @@ package contactManagerCore;
 import java.util.GregorianCalendar;
 import java.util.HashSet;
 
+import org.junit.Test;
 
-public class ContactManagerTest extends BasicTest{
+
+public class ContactManagerTest extends BasicTest {
 	ContactManagerImpl manager = new ContactManagerImpl();
 	HashSet<Contact> contacts = new HashSet<Contact>();
 	GregorianCalendar date = new GregorianCalendar();
 	
+	@Test
 	public void testAddFutureMeeting() {
-
+	valueExpected = manager.getFutureMeeting(0);
 	}
 }
