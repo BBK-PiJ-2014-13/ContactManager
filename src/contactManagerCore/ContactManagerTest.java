cc86462d78d35168dae322059cf947b25f571a02
Ilya Ivanov <contact@ilyaivanov.org>
2014-12-06 17:17:44 +0000

    Added fields "contacts" and "date"

--- src/contactManagerCore/ContactManagerTest.java
+++ src/contactManagerCore/ContactManagerTest.java
@@ -1,6 +1,11 @@
 package contactManagerCore;
 
+import java.util.GregorianCalendar;
+import java.util.HashSet;
+
 public class ContactManagerTest {
+	HashSet<Contact> contacts = new HashSet<Contact>();
+	GregorianCalendar date = new GregorianCalendar();
 	
 	public void testAddFutureMeeting() {
 		
