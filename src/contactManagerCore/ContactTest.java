0841fefab9e62a4aac5d79d41bee81c14ddac24c
Ilya Ivanov <contact@ilyaivanov.org>
2014-12-18 16:47:05 +0000

    Added @Test annotation to every method

--- src/contactManagerCore/ContactTest.java
+++ src/contactManagerCore/ContactTest.java
@@ -5,24 +5,28 @@ import org.junit.Test;
 public class ContactTest extends BasicTest {
 	ContactImpl contact = new ContactImpl(10, "John");
 
+	@Test
 	public void testsGetId() {
 		valueExpected = 10;
 		valueActual = contact.getId();
 		test();
 	}
 
+	@Test
 	public void testsGetName() {
 		valueExpected = "John";
 		valueActual = contact.getName();
 		test();
 	}
 
+	@Test
 	public void testsGetNotes() {
 		valueExpected = "";
 		valueActual = contact.getNotes();
 		test();
 	}
-
+	
+	@Test
 	public void testsAddNotes() {
 		contact.addNotes("notes");
 		valueExpected = "notes";
