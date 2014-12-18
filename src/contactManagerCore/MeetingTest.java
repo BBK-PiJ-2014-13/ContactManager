04c58861e210eddbe5f338ee349467973a16e5b5
Ilya Ivanov <contact@ilyaivanov.org>
2014-12-18 16:40:22 +0000

    Added @Test annotation to every method

--- src/contactManagerCore/MeetingTest.java
+++ src/contactManagerCore/MeetingTest.java
@@ -3,23 +3,28 @@ package contactManagerCore;
 import java.util.GregorianCalendar;
 import java.util.HashSet;
 
+import org.junit.Test;
+
 public class MeetingTest extends BasicTest {
 	HashSet<Contact> contact = new HashSet<Contact>();
 	GregorianCalendar calendar = new GregorianCalendar();
 	MeetingImpl meeting = new MeetingImpl(10, calendar, contact);
 
+	@Test
 	public void testsGetId() {
 		valueExpected = 10;
 		valueActual = meeting.getId();
 		test();
 	}
 	
+	@Test
 	public void testsGetDate() {
 		valueExpected = calendar;
 		valueActual = meeting.getDate();
 		test();
 	}
 	
+	@Test
 	public void testsGetContacts() {
 		valueExpected = contact;
 		valueActual = meeting.getContacts();
