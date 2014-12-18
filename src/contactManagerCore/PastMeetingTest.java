6611b9ed01d631df0a8e445d6ed9bdca886679c5
Ilya Ivanov <contact@ilyaivanov.org>
2014-12-18 16:48:25 +0000

    Added @Test annotation

--- src/contactManagerCore/PastMeetingTest.java
+++ src/contactManagerCore/PastMeetingTest.java
@@ -11,6 +11,7 @@ public class PastMeetingTest extends BasicTest{
 	String notes = "notes";
 	PastMeetingImpl meeting = new PastMeetingImpl(10, calendar, contact, notes);
 	
+	@Test
 	public void testsGetNotes() {
 		valueExpected = notes;
 		valueActual = meeting.getNotes();
