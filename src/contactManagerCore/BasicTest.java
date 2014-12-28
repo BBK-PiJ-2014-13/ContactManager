b284e41ec1bb8e30043bba0319fb3ab20bbbf962
Ilya Ivanov <contact@ilyaivanov.org>
2014-12-28 17:35:50 +0000

    changed valueExpected to be 1 and Actual 0

--- src/contactManagerCore/BasicTest.java
+++ src/contactManagerCore/BasicTest.java
@@ -6,8 +6,8 @@ import org.junit.Test;
 
 
 public class BasicTest {
-	private Object valueExpected = 0;
-	public Object valueActual = 1;
+	private Object valueExpected = 1;
+	public Object valueActual = 0;
 	
 	@Test
 	public void test() {
