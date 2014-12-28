1c596a7071d2904d22ca250ce82ec9933649240c
Ilya Ivanov <contact@ilyaivanov.org>
2014-12-28 17:58:28 +0000

    Added a contact to contacts

--- src/contactManagerCore/ContactManagerTest.java
+++ src/contactManagerCore/ContactManagerTest.java
@@ -71,6 +71,7 @@ public class ContactManagerTest extends BasicTest {
 
 	@Test
 	public void getPastMeetingTest() {
+		contacts.add(contact);
 		manager.addNewPastMeeting(contacts, calendar, "notes1");
 		manager.addNewPastMeeting(contacts, calendar, "notes2");
 		manager.addFutureMeeting(contacts, calendar);
