17c7f7639c1bcdc0a86377eab7e3158c4a0b58b7
Ilya Ivanov <contact@ilyaivanov.org>
2015-01-01 15:45:31 +0000

    Removed ArrayList from arguments

--- src/contactManagerCore/ContactManagerImpl.java
+++ src/contactManagerCore/ContactManagerImpl.java
@@ -404,7 +404,7 @@ public class ContactManagerImpl implements ContactManager {
 						Contact targetContact = new ContactImpl(id, name);
 						targetContact.addNotes(notes);
 						// TODO add code to prevent exceptions when lists are empty
-						addToList(id, targetContact, contactsList);
+						addToList(id, targetContact);
 
 					}
 				} else {
